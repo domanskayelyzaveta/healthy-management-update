4e7f392b493e967d1f2bd838f76474657ef1d749
Aleksandr2905 <natalukha@ukr.net>
2024-12-06 14:03:19 +0200

    update why choose us

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -11,6 +11,7 @@ import {
   Text,
 } from "./WhyChooseUs.styled";
 import sprite from "../../images/sprite.svg";
+import { dataWhyChooseUs } from "./DataWhyChooseUs";
 
 const WhyChooseUs = () => {
   return (
@@ -18,63 +19,19 @@ const WhyChooseUs = () => {
       <ContentWrapper>
         <ContentTitle>Чому обирають нас</ContentTitle>
         <List>
-          <Item>
-            <DivSvg>
-              <Svg>
-                <use href={`${sprite}#hands-shake`}></use>
-              </Svg>
-            </DivSvg>
-            <Div>
-              <ItemTitle>Надійне партнерство та менторство</ItemTitle>
-              <Text>
-                Ми налаштовані на довгострокове партнерство і дбаємо про
-                комфортні та чесні комунікації
-              </Text>
-            </Div>
-          </Item>
-          <Item>
-            <DivSvg>
-              <Svg>
-                <use href={`${sprite}#setting`}></use>
-              </Svg>
-            </DivSvg>
-            <Div>
-              <ItemTitle>Індивідуальний підхід</ItemTitle>
-              <Text>
-                Кожен бізнес і послуга мають свої особливості. Ми занурюємося у
-                ваш бізнес і пропонуємо індивідуальні стратегії, які допоможуть
-                вам досягти успіху
-              </Text>
-            </Div>
-          </Item>
-          <Item>
-            <DivSvg>
-              <Svg>
-                <use href={`${sprite}#icon-lamp`}></use>
-              </Svg>
-            </DivSvg>
-            <Div>
-              <ItemTitle>Експертні послуги</ItemTitle>
-              <Text>
-                Ми опираємося на передовий досвід і використовуємо як класичні,
-                так і сучасні підходи для досягнення поставленого результату
-              </Text>
-            </Div>
-          </Item>
-          <Item>
-            <DivSvg>
-              <Svg>
-                <use href={`${sprite}#icon-puzzle`}></use>
-              </Svg>
-            </DivSvg>
-            <Div>
-              <ItemTitle>Комплексні рішення</ItemTitle>
-              <Text>
-                Наша експертиза дозволяє пропонувати комплексні рішення для
-                ефективної співпраці
-              </Text>
-            </Div>
-          </Item>
+          {dataWhyChooseUs.map((item) => (
+            <Item key={item.icon}>
+              <DivSvg>
+                <Svg>
+                  <use href={`${sprite}#${item.icon}`}></use>
+                </Svg>
+              </DivSvg>
+              <Div>
+                <ItemTitle>{item.title}</ItemTitle>
+                <Text>{item.text}</Text>
+              </Div>
+            </Item>
+          ))}
         </List>
       </ContentWrapper>
     </SectionChoose>
